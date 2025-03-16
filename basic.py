29526b7d631ea9392a1c1ad843790468d42189f4
shia <nahsor2242195@gmail.com>
2025-03-17 06:53:00 +0900

    basic

---
+++ basic.py
@@ -0,0 +1,12 @@
+print('hello world')
+
+#標準入力
+name = input('what your name\n')
+print(name)
+age = 18
+
+#format関数
+print('my name is {}, my age is {}'.format(name,age))
+
+a = b = 'hello'
+print(a, b)
